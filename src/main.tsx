342d7bb08ebefa218dbb0f83329e570e6890fe73
Marlon Felix <marlonsantos@kirvano.com>
2024-10-16 11:41:06 -0300

    att

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from '@/ui'
+
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
+
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/ui/Provider/Provider.tsx
+++ src/ui/Provider/Provider.tsx
@@ -1,7 +1,9 @@
 import { NextUIProvider } from "@nextui-org/system";
 import { useNavigate } from "react-router-dom";
 
-export function Provider({ children }: { children: React.ReactNode }) {
+import { ProviderProps } from './Provider.types'
+
+export const Provider = ({ children }: ProviderProps) => {
   const navigate = useNavigate();
 
   return <NextUIProvider navigate={navigate}>{children}</NextUIProvider>;
