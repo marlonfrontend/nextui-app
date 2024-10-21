4b94ca6b270b9de115ee2c07212e6416eeb3d991
Marlon Felix <contatomarlonfelix@hotmail.com>
2024-10-21 18:38:50 -0300

    att

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -4,6 +4,7 @@ import { Layout, Navbar } from '@/ui'
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Layout>
+      <Layout.Aside>sidebar</Layout.Aside>
       <Layout.Header>
         <Navbar maxWidth="xl">
           <Navbar.Brand>
@@ -15,28 +16,9 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
           </Navbar.Content>
         </Navbar>
       </Layout.Header>
-      <Layout.Content>{children}</Layout.Content>
+      <Layout.Main>{children}</Layout.Main>
       <Layout.Footer>copyright</Layout.Footer>
     </Layout>
-    // <div className="relative flex flex-col h-screen">
-    // <Navbar maxWidth="xl">
-    //   <Navbar.Brand>
-    //     <Logo />
-    //   </Navbar.Brand>
-    //   <Navbar.Content>
-    //     <Navbar.Item>Home</Navbar.Item>
-    //     <Navbar.Item>About</Navbar.Item>
-    //   </Navbar.Content>
-    // </Navbar>
-    //   <Progress
-    //     classNames={{ track: 'rounded-none' }}
-    //     color="primary"
-    //     size="sm"
-    //     value={20}
-    //   />
-    //   <main className="w-full flex-grow pt-16">{children}</main>
-    //   <footer className="w-full flex items-center justify-center py-3" />
-    // </div>
   )
 }
 
--- src/ui/Layout/Layout.tsx
+++ src/ui/Layout/Layout.tsx
@@ -1,20 +1,31 @@
 /* eslint-disable react/display-name */
 import { PropsWithChildren } from 'react'
 
-import { LayoutProps } from './Layout.types'
+import { LayoutProps, LayoutHeaderProps } from './Layout.types'
 
 export const Layout = ({ children }: PropsWithChildren<LayoutProps>) => {
-  return <div>{children}</div>
+  return <div className="relative flex flex-col h-screen">{children}</div>
 }
 
 Layout.Header = ({ children }: PropsWithChildren<LayoutProps>) => {
   return <header>{children}</header>
 }
 
-Layout.Content = ({ children }: PropsWithChildren<LayoutProps>) => {
-  return <main className="">{children}</main>
+Layout.Main = ({ children }: PropsWithChildren<LayoutProps>) => {
+  return <main className="w-full flex-grow">{children}</main>
 }
 
 Layout.Footer = ({ children }: PropsWithChildren<LayoutProps>) => {
   return <footer>{children}</footer>
+}
+
+Layout.Aside = ({
+  children,
+  ...props
+}: PropsWithChildren<LayoutHeaderProps>) => {
+  return (
+    <aside className="bg-gray-100 w-[200px] fixed h-full" {...props}>
+      {children}
+    </aside>
+  )
 }
