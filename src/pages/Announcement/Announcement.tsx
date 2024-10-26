fc5f9df889e2b5e3827af68b0f95fcbe774df233
Marlon Felix <contatomarlonfelix@hotmail.com>
2024-10-26 11:14:52 -0300

    att

--- src/pages/Announcement/Announcement.tsx
+++ src/pages/Announcement/Announcement.tsx
@@ -1,5 +1,5 @@
 import AnnouncementLayout from '@/layouts/announcement'
-import { Button, Container, Form, Input, Spacer } from '@/ui'
+import { Container } from '@/ui'
 
 export const AnnouncementPage = () => {
   return (
@@ -9,31 +9,8 @@ export const AnnouncementPage = () => {
           <div className="text-3xl font-medium">
             Informe a localização do seu espaço
           </div>
-          <Spacer y={10} />
-          <Form onSubmit={() => console.log('enviar')}>
-            <Input label="CEP" />
-            <Spacer y={5} />
-            <Input label="Endereço" />
-
-            <Spacer y={10} />
-            <div className="flex justify-between mt-10">
-              <Button color="primary" size="lg" variant="light">
-                Cancelar
-              </Button>
-              <div className="flex gap-6">
-                <Button color="primary" size="lg" variant="bordered">
-                  Voltar
-                </Button>
-                <Button color="primary" size="lg">
-                  Continuar
-                </Button>
-              </div>
-            </div>
-          </Form>
         </div>
       </Container>
     </AnnouncementLayout>
   )
 }
-
-// export default AnnouncementPage
