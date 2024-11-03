b81fb1b8ab2639791451bb61bbed39aaeed434f5
Marlon Felix <contatomarlonfelix@hotmail.com>
2024-11-03 15:57:13 -0300

    att

--- src/components/EventCategories/EventCategories.styles.tsx
+++ src/components/EventCategories/EventCategories.styles.tsx
@@ -1,10 +1,10 @@
 import { tv } from 'tailwind-variants'
 
 export const eventCategoriesVariants = tv({
-  base: 'cursor-pointer py-2 px-3 border rounded-full text-sm transition-all duration-250 text-nowrap',
+  base: 'dark:border-divider cursor-pointer py-2 px-3 border rounded-full text-sm transition-all duration-250 !text-nowrap',
   variants: {
     selected: {
-      true: 'bg-black text-white border-black',
+      true: 'bg-black dark:border-white dark:bg-white dark:text-black text-white border-black',
       false: 'hover:border-black'
     }
   },
