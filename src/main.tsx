ead8c876566f592ab876da0af5ba048b0bb75091
Marlon Felix <contatomarlonfelix@hotmail.com>
2024-11-06 23:54:40 -0300

    att

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
+        {/* teste */}
         <App />
       </Provider>
     </BrowserRouter>
